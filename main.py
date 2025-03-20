662bc2dd95b3d56d8687cd937ef938e2c773dcbd
Elio <nguyen2332004@gmail.com>
2025-03-20 09:21:58 +0700

    data ingestion

---
+++ main.py
@@ -0,0 +1,16 @@
+# Testing module
+from src.GAN import logger
+#logger.info("Welcome to our custom log")
+
+# Test 01
+from src.GAN.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/GAN/entity/config_entity.py
@@ -0,0 +1,11 @@
+# base on params.yaml and config/config.yaml do:
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
